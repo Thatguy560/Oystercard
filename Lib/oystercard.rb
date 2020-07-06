6c0363e0edf56b54bd1beb69f4737fb3c22a8ed9
Peter Stevens <Peterstevens510@gmail.com>
2020-07-06 21:36:41 +0100

    slight README ammendments

--- Lib/oystercard.rb
+++ Lib/oystercard.rb
@@ -6,11 +6,11 @@ class Oystercard
   attr_reader :balance, :current_trip, :journey, :entry_station, :exit_station
   DEFAULT_BALANCE = 0
   MINIMUM_BALANCE = 1
-  MAXIMUM_BALANCE = 90 
+  MAXIMUM_BALANCE = 90
 # Instance variables don’t have to be created in the initialize method, but that’s how you give them an initial value
-  def initialize(balance = DEFAULT_BALANCE) # this initialises every oystercard with balance of 0 
+  def initialize(balance = DEFAULT_BALANCE) # this initialises every oystercard with balance of 0
     @balance = balance
-    @current_trip = nil 
+    @current_trip = nil
     @journeys = []
   end
 
@@ -31,7 +31,7 @@ class Oystercard
   end
 
   def touch_out(exit_station)
-    if @current_trip == nil 
+    if @current_trip == nil
     new_journey # Saves new journey even if you don't tap in at entry station.
     end
     @current_trip.finished_journey(exit_station)
@@ -41,10 +41,10 @@ class Oystercard
   end
 
   def journey_history
-    @journeys.empty? ? "No Journeys have been made yet on this Oystercard" : @journeys.each { |journey| puts journey }
+    @journeys.empty? ? "No journeys have been made yet" : @journeys.each { |journey| puts journey }
   end
 
-  private 
+  private
 
   def deduct(amount)
     @balance -= amount
@@ -54,9 +54,9 @@ class Oystercard
     @balance == MAXIMUM_BALANCE
   end
 
-  def insufficient_funds 
+  def insufficient_funds
     @balance < MINIMUM_BALANCE
-  end 
+  end
 
   def add_journey
     @journeys << @current_trip
@@ -69,10 +69,8 @@ class Oystercard
   def new_journey
     @current_trip = Journey.new
   end
-  
+
 end
 
-# Once you've made a method private, 
+# Once you've made a method private,
 # you no longer need to have a test for it
-
-# !! (Double bang) converts the value to a boolean.
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -2,12 +2,12 @@ require 'oystercard'
 require 'journey'
 require 'station'
 
-describe Oystercard do 
+describe Oystercard do
   let(:oystercard) { Oystercard.new }
   let(:entry_station){ double :entry_station}
   let(:exit_station){ double :exit_station}
-  
-  it "will let you top up your Oystercard" do 
+
+  it "will let you top up your Oystercard" do
     subject.top_up(5)
     expect(subject.balance).to eq(5)
   end
@@ -17,37 +17,37 @@ describe Oystercard do
   end
 
   context "for making journeys with your Oystercard" do # #creating a context block so the before block (that is right below this message) doesnt get applied to
-    #the above tests. 
+    #the above tests.
     before do
       subject.top_up(Oystercard::MAXIMUM_BALANCE)
-    end 
+    end
 
-    it "has a maximum balance of £90." do 
+    it "has a maximum balance of £90." do
       expect{subject.top_up(1)}.to raise_error "Maximum balance of £#{Oystercard::MAXIMUM_BALANCE} reached"
     end
 
-    it "will allow you to check the Oystercard balance at any time." do 
+    it "will allow you to check the Oystercard balance at any time." do
       expect {subject.check_balance}.to output("Your current Oystercard balance is £#{subject.balance}.00\n").to_stdout
     end
-    
-    it "expects to know where I've travelled from" do 
+
+    it "expects to know where I've travelled from" do
       subject.touch_in("Enfield Town")
       expect(subject.current_trip.entry_station).to eq("Enfield Town")
     end
-    
-    it "will save a journey if you touch in and out at the station" do 
+
+    it "will save a journey if you touch in and out at the station" do
       subject.touch_in("Enfield Town")
       subject.touch_out("London Liverpool Street")
       expect(subject.journey_history.length).to eq 1
-    end 
-    
-    it "will also save a journey if you forget to touch in" do 
+    end
+
+    it "will also save a journey if you forget to touch in" do
       subject.touch_out("London Liverpool Street")
       expect(subject.journey_history.length).to eq 1
-    end 
+    end
 
-    it "if no journeys have been made yet it will tell you" do 
-      expect(subject.journey_history).to eq "No Journeys have been made yet on this Oystercard"
+    it "if no journeys have been made yet it will tell you" do
+      expect(subject.journey_history).to eq "No journeys have been made yet"
     end
 
   end
